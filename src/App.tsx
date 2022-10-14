a5f4a4cc29dfca482913ba0a1a26f897c5df132e
cj-nassif <nassif.cj@gmail.com>
2022-10-14 07:33:23 -0300

    inicial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css';
+
+export function App() {
+  
+  return (
+    <div>
+      <h1 className='text-2xl'>Hello World</h1>
+      <button className='h-16 w-32 bg-cyan-500 rounded text-white hover:bg-cyan-300'>Enviar</button>
+    </div>
+  )
+}
+
+
---
+++ storybook-static/assets/preview.85747790.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.fc172c4e.js";import"./iframe.464f1316.js";import"./es.map.constructor.5b2aacaa.js";import"./es.number.to-fixed.928a03d1.js";import"./index.f7977393.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.85747790.js.map
